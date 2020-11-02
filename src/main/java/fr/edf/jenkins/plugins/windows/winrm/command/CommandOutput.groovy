eaf042a0bb7620f338d4e3ec77080bf017c9db00
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2020-11-02 18:40:39 +0100

    Adding EDF license on own code

--- src/main/java/fr/edf/jenkins/plugins/windows/winrm/command/CommandOutput.groovy
+++ src/main/java/fr/edf/jenkins/plugins/windows/winrm/command/CommandOutput.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2015 Aestas/IT, 2020 EDF
+ * Copyright (C) 2011-2015 Aestas/IT, 2020 EDF Group and its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
