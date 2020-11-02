39e50608bfec0f956307a667faf23f0dc1929a83
Mathieu Delrocq <delrocq.mathieu@gmail.com>
2020-11-02 14:24:38 +0100

    Add a formatted message to WinRMException

--- src/main/java/fr/edf/jenkins/plugins/windows/winrm/WinRMException.groovy
+++ src/main/java/fr/edf/jenkins/plugins/windows/winrm/WinRMException.groovy
@@ -2,6 +2,8 @@ package fr.edf.jenkins.plugins.windows.winrm
 
 class WinRMException extends Exception {
     
+    public static final String FORMATTED_MESSAGE = "Error during %s command, protocol : %s, code : %s, description : %"
+    
     private WinRMException() {
         // Hide default constructor
     }
